94279e00ac9667654ae5c594f5d5c359b6a1bdc4
Yash-Garg <yash.garg@outlook.in>
2022-06-29 00:19:30 +0530

    chore: use debug signing config for now

--- test/ui/home/cubit/home_cubit_test.dart
+++ test/ui/home/cubit/home_cubit_test.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:petperfect_exercise/data/injection/injectable.dart';
 import 'package:petperfect_exercise/ui/home/cubit/home_cubit.dart';
 
 void main() {
@@ -13,6 +14,15 @@ void main() {
       build: HomeCubit.new,
       act: (cubit) => cubit.processed(),
       expect: () => [HomeState.processed()],
+    );
+
+    blocTest<HomeCubit, HomeState>(
+      'emits loaded state when the media is fetched',
+      setUp: () => configureDependencies(),
+      wait: Duration(seconds: 5),
+      build: HomeCubit.new,
+      act: (cubit) => cubit.started(),
+      expect: () => [HomeState.loaded()],
     );
   });
 }
